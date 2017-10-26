a726e9f3024809fd3d94094b3026e6ca78d59b88
Stefanos Mousafeiris <sm6114@imperial.ac.uk>
2017-10-27 00:07:18 +0100

    added convexity test
    seems like F_3 is not convex

--- moments.py
+++ moments.py
@@ -2,7 +2,7 @@ import qutip as q
 import numpy as np
 from scipy import integrate as ig
 import matplotlib.pyplot as plt
-N=2
+N=3
 
 # Generate coherent measurement state.
 m_state = q.basis(N,0) * 1./(np.sqrt(N))
@@ -16,22 +16,19 @@ for i in range(1,N):
 system = q.rand_dm_ginibre(N, rank=1)
 
 # Generate random density matrix.
-#system = q.rand_dm(N, 0.75)
+system = q.rand_dm(N, 1)
+
+# Generate maximally coherent density matrix.
+#system = q.ket2dm(m_state)
 
 H = q.charge(N,1)
-print(system)
+#print(system)
 
-a=np.diag(np.sqrt(system.diag())*m_state)
+#a=np.diag(np.sqrt(system.diag())*m_state)
 #print(np.abs(np.vdot(a,a)))
 
 def prob_dist(t):
     m_state_t = q.sesolve(H, m_state,[0.,t]).states[1]
-    #print(t/(np.pi))
-    #print(m_state)
-    #print(m_state_t)
-    #print("~~~~")
-    #print(system)
-    #print(q.expect(system,m_state_t))
     return q.expect(system,m_state_t)
 
 def integrand(t,n):
@@ -40,31 +37,54 @@ def integrand(t,n):
 
 def moment(n):
     M = ig.quad(integrand,0.,2*np.pi,(n))
-    return M[0]
+    return (1/(2*np.pi))*M[0]
 
 
 def mom_func3(c=1):
     m1 = moment(1)
     m2 = moment(2)
     m3 = moment(3)
-    print(m1,m2,m3)
+    #print(8*c/243.)
     return 2*c*m1*m1*m1 -3*c*m2*m1 + c*m3
     
 
 
 #print(moment(2))
-print(mom_func3())    
-
-
-b = []
-t_list =np.linspace(0.,2*np.pi,200)
-for t in t_list:
-    b.append(prob_dist(t))
-
-plt.plot(t_list,b)
-plt.xlim(0,2*np.pi)
-plt.ylim(0,1)
+#print(mom_func3())
+#print(8/243.)
 
 
 
+
+
+def plot_pattern():
+    b = []
+    t_list =np.linspace(0.,2*np.pi,200)
+    for t in t_list:
+        b.append(prob_dist(t))        
+    plt.plot(t_list,b)
+    plt.xlim(0,2*np.pi)
+    plt.ylim(0,1)
+
+
+def convex_test():
+    p = np.random.rand()
+    rho1 = q.rand_dm(N, 1)
+    rho2 = q.rand_dm(N, 1)
+    combined  = p*rho1 + (1.-p)*rho2
     
+    global system
+    system = rho1
+    a = p*mom_func3()
+    system = rho2
+    b = (1-p)*mom_func3()
+    system = combined
+    c = mom_func3()
+    
+    if (a+b < c):
+        print("FAILED")
+        print(a+b)
+        print(a,b)
+        print(c)
+    
+convex_test()
