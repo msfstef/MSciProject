e383f156e65506d0d5c4bd4b699570ad32c8ed22
Stefanos Mousafeiris <sm6114@imperial.ac.uk>
2017-11-06 19:19:22 +0000

    Minor additions

--- moments.py
+++ moments.py
@@ -4,38 +4,46 @@ from scipy import integrate as ig
 import matplotlib.pyplot as plt
 N=3
 
+
+def max_coherent(k):
+    state = q.basis(N,0) * 1./(np.sqrt(N))
+    for i in range(1,k):
+        state += q.basis(N,i) * 1./(np.sqrt(N))
+    return state
+
+
+
 # Generate coherent measurement state.
-m_state = q.basis(N,0) * 1./(np.sqrt(N))
-for i in range(1,N):
-    m_state += q.basis(N,i) * 1./(np.sqrt(N))
+m_state = max_coherent(N)
 
 # Generate random measurement state.
-m_state = q.rand_ket(N)
+#m_state = q.rand_ket(N)
 
 # Generate pure state density matrix.
 system = q.rand_dm_ginibre(N, rank=1)
 
 # Generate random density matrix.
-system = q.rand_dm(N, 1)
+#system = q.rand_dm(N, 1)
 
 # Generate maximally coherent density matrix.
-#system = q.ket2dm(m_state)
+#system = q.ket2dm(max_coherent(N))
 
 
-state1 = (1/np.sqrt(2))*(q.basis(3,1) + q.basis(3,0))
-state2 = (1/np.sqrt(2))*(q.basis(3,2) + q.basis(3,0))
-print(state1)
-print(state2)
-system1 = q.ket2dm(state1)
-system2 = q.ket2dm(state2)
-system = 0.5*system1 + 0.5*system2
+#Testing for 2-coherent mixed states.
+#state1 = (1/np.sqrt(2))*(q.basis(3,1) + q.basis(3,0))
+#state2 = (1/np.sqrt(2))*(q.basis(3,2) + q.basis(3,0))
+#system1 = q.ket2dm(state1)
+#system2 = q.ket2dm(state2)
+#system = 0.5*system1 + 0.5*system2
+
 
 H = q.charge(N,1)
 print(system)
 
 
 def prob_dist(t):
-    m_state_t = q.sesolve(H, m_state,[0.,t]).states[1]
+    m_state_t = q.sesolve(H, m_state,[0.,t]).states[1] * np.sqrt(N)
+    #print(m_state_t)
     return q.expect(system,m_state_t)
 
 def integrand(t,n):
@@ -47,19 +55,24 @@ def moment(n):
     return (1/(2*np.pi))*M[0]
 
 
-def mom_func3(c=100):
+def mom_product(n1=1, n2=2):
+    return moment(n1)*moment(n2)
+
+
+def mom_func3_OLD(c=100):
     m1 = moment(1)
     m2 = moment(2)
     m3 = moment(3)
     #print(8*c/243.)
     return 2*c*m1*m1*m1 -3*c*m2*m1 + c*m3
-    
-def mom_product(n1=1, n2=2):
-    return moment(n1)*moment(n2)
 
-#print(moment(2))
-print(mom_func3())
-#print(8/243.)
+def mom_func3(a,b,c):
+    m1 = moment(1)
+    m2 = moment(2)
+    m3 = moment(3)
+    print(m1,m2,m3)
+    #print(8*c/243.)
+    return a*m1*m1*m1 +b*m2*m1 + c*m3
 
 
 
@@ -72,10 +85,10 @@ def plot_pattern():
         b.append(prob_dist(t))        
     plt.plot(t_list,b)
     plt.xlim(0,2*np.pi)
-    plt.ylim(0,1)
+    plt.ylim(0,N)
 
 
-def convex_test(func=mom_func3):
+def convex_test(func, *args):
     p = np.random.rand()
     rho1 = q.rand_dm(N, 1)
     rho2 = q.rand_dm(N, 1)
@@ -83,11 +96,11 @@ def convex_test(func=mom_func3):
     
     global system
     system = rho1
-    a = p*func()
+    a = p*func(*args)
     system = rho2
-    b = (1-p)*func()
+    b = (1-p)*func(*args)
     system = combined
-    c = func()
+    c = func(*args)
     
     if (a+b < c):
         print("FAILED")
@@ -96,6 +109,9 @@ def convex_test(func=mom_func3):
         print(c)
 
 
-#for i in range(100):
-#    print(i)
-#    convex_test(mom_product)
+#plot_pattern()
+
+
+for i in range(100):
+    print(i)
+    convex_test(mom_func3, -1,-1,2)
